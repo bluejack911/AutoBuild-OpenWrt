822c2ba0792605b1fe0505830555e8577d5db77a
bluejack911 <49217273+bluejack911@users.noreply.github.com>
2024-06-03 12:37:15 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,10 +6,11 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/root::0:0:99999:7:::/root:$1$3ZL2YxPA$NmwUNvXNWmpdhlQV8xQcp1:19872:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
